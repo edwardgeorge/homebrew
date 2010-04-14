c461740d794f100e09a82f835691fc40bbe93731
Edward George <edwardgeorge@gmail.com>
2010-04-14 10:08:09 +0100

    New formula: libnids 1.24

---
+++ Library/Formula/libnids.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Libnids <Formula
+  url 'http://downloads.sourceforge.net/project/libnids/libnids/1.24/libnids-1.24.tar.gz'
+  homepage 'http://libnids.sourceforge.net/'
+  md5 '72d37c79c85615ffe158aa524d649610'
+
+  depends_on 'libpcap'
+  depends_on 'libnet'
+  depends_on 'pkg-config'
+  depends_on 'glib'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
