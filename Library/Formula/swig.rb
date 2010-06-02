187f13f401ae603c779e4b52e7b86ea0da598226
Edward George <edwardgeorge@gmail.com>
2010-06-02 21:12:14 +0100

    added keg_only swig (1.3.40).
    SWIG is an interface compiler that connects programs written in C and
    C++ with scripting languages such as Perl, Python, Ruby, and Tcl.

---
+++ Library/Formula/swig.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Swig <Formula
+  url 'http://downloads.sourceforge.net/project/swig/swig/swig-1.3.40/swig-1.3.40.tar.gz'
+  homepage 'http://www.swig.org/'
+  md5 '2df766c9e03e02811b1ab4bba1c7b9cc'
+
+  def keg_only?
+    "provided by osx but doesn't work with non-system python."
+  end
+
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
