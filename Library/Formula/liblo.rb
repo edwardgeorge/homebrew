e5083e630f33792e7565d95fb5ea18309aa78c96
Edward George <edwardgeorge@gmail.com>
2010-08-06 22:59:03 +0100

    missing md5 for liblo 0.26

--- Library/Formula/liblo.rb
+++ Library/Formula/liblo.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Liblo <Formula
   url 'http://downloads.sourceforge.net/project/liblo/liblo/0.26/liblo-0.26.tar.gz'
   homepage 'http://liblo.sourceforge.net/'
-  md5 ''
+  md5 '5351de14262560e15e7f23865293b16f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
