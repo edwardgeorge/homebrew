f09bcec73b534754bee0df2e18409a52fdc43301
Edward George <edwardgeorge@gmail.com>
2010-10-21 00:32:14 +0100

    updated c10t to 1.1

--- Library/Formula/c10t.rb
+++ Library/Formula/c10t.rb
@@ -1,21 +1,21 @@
 require 'formula'
 
 class C10t <Formula
-  url 'http://github.com/udoprog/c10t/tarball/1.0'
+  url 'http://github.com/udoprog/c10t/tarball/1.1'
   homepage 'http://github.com/udoprog/c10t'
-  md5 '0c947c6befef79b040858c1ed308c0d6'
+  md5 'e03316bd5554a32a65979ff8785b8a44'
 
   depends_on 'cmake'
   depends_on 'libpng'
   depends_on 'boost'
 
   def install
-    #system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          #"--prefix=#{prefix}"
+    inreplace 'src/global.h', "font.ttf", "#{prefix}/font.ttf"
     inreplace 'CMakeLists.txt', 'boost_thread', 'boost_thread-mt'
     inreplace 'test/CMakeLists.txt', 'boost_unit_test_framework', 'boost_unit_test_framework-mt'
     system "cmake . #{std_cmake_parameters}"
     system "make"
     bin.install "c10t"
+    prefix.install "font.ttf"
   end
 end
