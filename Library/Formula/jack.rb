1299ef717ee4a919b27afbb8af9a182e60c87eb4
Edward George <edwardgeorge@gmail.com>
2010-11-08 14:16:19 +0000

    Update jack formula to use binaries to get missing parts.

--- Library/Formula/jack.rb
+++ Library/Formula/jack.rb
@@ -1,22 +1,63 @@
 require 'formula'
 
 class Jack <Formula
-  url 'http://www.grame.fr/~letz/jack-1.9.6.tar.bz2'
   homepage 'http://jackaudio.org'
-  md5 'fcd4bdb44f51d71c9e19bc1d0e887902'
-
-  depends_on 'celt'
-  depends_on 'libsamplerate'
-
-  def patches
-    # default build assumes ppc+i386, changed to i386+x86_64
-    "http://gist.github.com/raw/636194/jack-1.9.6_homebrew.patch"
+  version '0.87'
+  if MACOS_VERSION < 10.6
+    url 'http://downloads.sourceforge.net/project/jackosx/JackOSX/0.87/JackOSX.0.87_32bits.zip'
+    md5 '431b35cab4b3928e66e8accb2a1dee80'
+  else
+    url 'http://downloads.sourceforge.net/project/jackosx/JackOSX/0.87/JackOSX.0.87_64-32bits.zip'
+    md5 'e1cb49d14659db0ce23f00977542a28e'
   end
 
   def install
-    ENV['LINKFLAGS'] = ENV['LDFLAGS']
-    system "./waf","configure","--prefix=#{prefix}"
-    system "./waf","build"
-    system "./waf","install"
+    if MACOS_VERSION < 10.6
+      system "xar -xf JackOSX.0.87_32bits.pkg"
+    else
+      system "xar -xf JackOSX.0.87_64-32bits.pkg"
+    end
+    system "pax -rz -f jack.pkg/Payload"
+    Dir['usr/local/bin/jack_*',
+        'Library/Audio/Plug-Ins/HAL/JackRouter.plugin/Contents/MacOS/JackRouter',
+        'Library/Audio/Plug-Ins/Components/JACK-insert.component/Contents/MacOS/JACK-insert',
+        'Library/Audio/Plug-Ins/VST/JACK-insert.vst/Contents/MacOS/JACK-insert',
+        'Library/Frameworks/Jackmp.framework/Jackmp',
+        'Library/Frameworks/Jackmp.framework/Versions/A/Jackmp'].each do |f|
+      system "install_name_tool", "-change",
+        "/Library/Frameworks/Jackmp.framework/Versions/A/Jackmp",
+        prefix+"Frameworks/Jackmp.framework/Versions/A/Jackmp", f
+    end
+    Dir['usr/local/bin/jackd*', 'usr/local/lib/jackmp/*.so',
+        '/Library/Frameworks/Jackservermp.framework/Versions/A/Jackservermp',
+        '/Library/Frameworks/Jackservermp.framework/Jackservermp'].each do |f|
+      system "install_name_tool", "-change",
+        "/Library/Frameworks/Jackservermp.framework/Versions/A/Jackservermp",
+        prefix+"Frameworks/Jackservermp.framework/Versions/A/Jackservermp", f
+    end
+    lib.install Dir['usr/local/lib/*']
+    bin.install Dir['usr/local/bin/*']
+    include.install Dir['usr/local/include/*']
+    prefix.install 'Library/Frameworks'
+    prefix.install 'Library/Audio'
+    ln_s prefix+'Frameworks/Jackmp.framework/Jackmp',
+         lib+'libjack.0.dylib', :force => true
+    ln_s prefix+'Frameworks/Jackmp.framework/Jackmp',
+         lib+'libjack.dylib', :force => true
+    ln_s prefix+'Frameworks/Jackservermp.framework/Jackservermp',
+         lib+'libjackserver.0.dylib', :force => true
+    ln_s prefix+'Frameworks/Jackservermp.framework/Jackservermp',
+         lib+'libjackserver.dylib', :force => true
+  end
+
+  def caveats; <<-EOS
+The JackRouter CoreAudio device is located in jack/0.87/Audio/Plug-Ins/HAL/JackRouter.plugin
+within the Homebrew Cellar. Link this into /Library/Audio to install (requires restart.)
+
+ie: sudo ln -s #{HOMEBREW_PREFIX}/Cellar/jack/0.87/Audio/Plug-Ins/HAL/JackRouter.plugin /Library/Audio/Plug-Ins/HAL/
+(you make need to make the parent directories first.)
+
+Also included within the Audio subfolder are an AudioUnit and VST plugin.
+  EOS
   end
 end
